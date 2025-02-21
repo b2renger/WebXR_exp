afa0bef7dd634ed68c7f20af66148773d7a8c7f6
b2renger <berenger.recoules@gmail.com>
2025-02-21 16:22:10 +0100

    keep only cubes

--- ARThreejs_lego_physics/script.js
+++ ARThreejs_lego_physics/script.js
@@ -76,15 +76,16 @@ async function init() {
     scene.add(group);
 
     const geometries = [
-        new THREE.BoxGeometry(0.2, 0.2, 0.2),
+        new THREE.BoxGeometry(0.2, 0.2, 0.2)
+        /*,
         new THREE.ConeGeometry(0.2, 0.2, 64),
         new THREE.CylinderGeometry(0.2, 0.2, 0.2, 64),
         new THREE.IcosahedronGeometry(0.2, 8),
-        new THREE.TorusGeometry(0.2, 0.04, 64, 32)
+        new THREE.TorusGeometry(0.2, 0.04, 64, 32)*/
     ];
 
     for (let i = 0; i < 50; i++) {
-        const geometry = geometries[Math.floor(Math.random() /* geometries.length*/)];
+        const geometry = geometries[Math.floor(Math.random() * geometries.length)];
         const material = new THREE.MeshStandardMaterial({
             color: Math.random() * 0xffffff,
             roughness: 0.7,
